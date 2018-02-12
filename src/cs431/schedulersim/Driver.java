9a4477524910334ec28f92393388fadb81b4a129
Dimipilo92 <pilodi12@gmail.com>
2018-02-11 19:00:13 -0800

    Add more error checking for arguments

--- src/cs431/schedulersim/Driver.java
+++ src/cs431/schedulersim/Driver.java
@@ -10,35 +10,11 @@ public class Driver {
 		Driver.run(args);
 	}
 	
-	private static String absoluteDirectory(String relativeDir) {
-		String absoluteDirectory;
-		if (relativeDir.charAt(0) == 'C') {
-			absoluteDirectory = relativeDir;
-		}
-		else {
-			String callingPath = System.getProperty("user.dir");
-			absoluteDirectory = callingPath+"\\"+relativeDir;
-		}
-		return absoluteDirectory;
-	}
-	
-	public static File[] getTestFiles(String inputDirectory) {
-		File directory = new File(inputDirectory);
-		return directory.listFiles();
-	}
-	
-	public static Scheduler[] getSchedulers() {
-		return new Scheduler[]{
-				new FCFS(),
-				new SJF(),
-				new RoundRobin(25),
-				new RoundRobin(50),
-				new Lottery(25)};
-	}
-	
 	public static void run(String[] args) throws Exception{
-		String inputDirectory = absoluteDirectory(args[0]);
-		String outputDirectory = absoluteDirectory(args[1]);
+		String[] ioDirectories = getDirectoriesFromArguments(args);
+		String inputDirectory = ioDirectories[0];
+		String outputDirectory = ioDirectories[1];
+		
 		File[] testFiles = getTestFiles(inputDirectory);
 		Scheduler[] schedulerList = getSchedulers();
 
@@ -55,10 +31,60 @@ public class Driver {
 			}
 			sb.append("\n");
 		}
-		
-		System.out.println(sb.toString());
 		PrintWriter pw = new PrintWriter(new File(outputDirectory+"\\averages.csv"));
 		pw.write(sb.toString());
 		pw.close();
+	}
+	
+	private static File[] getTestFiles(String inputDirectory) {
+		File directory = new File(inputDirectory);
+		return directory.listFiles();
+	}
+	
+	private static Scheduler[] getSchedulers() {
+		return new Scheduler[]{
+				new FCFS(),
+				new SJF(),
+				new RoundRobin(25),
+				new RoundRobin(50),
+				new Lottery(25)};
+	}
+	
+	private static String[] getDirectoriesFromArguments(String[] args) {
+		String dirs[] = {};
+		try {
+			String inputDirectory = absoluteDirectory(args[0]);
+			String outputDirectory = absoluteDirectory(args[1]);
+			validateDirectory(inputDirectory);
+			validateDirectory(outputDirectory);
+			dirs = new String[]{inputDirectory, outputDirectory};
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Requires two arguments: \n" +
+					"\t1) input directory(absolute or relative) - where test files are stored\n" +
+					"\t2) output directory(absolute or relative) - where results will be stored");
+			System.exit(1);
+		}
+		return dirs;
+	}
+	
+	private static void validateDirectory(String dir){
+		File f = new File(dir);
+		if (!f.isDirectory()) {
+			System.out.println(f.getAbsolutePath()+" is not a valid directory!");
+			System.exit(1);
+		}
+	}
+	
+	private static String absoluteDirectory(String relativeDir) {
+		String absoluteDirectory;
+		if (relativeDir.charAt(0) == 'C') {
+			absoluteDirectory = relativeDir;
+		}
+		else {
+			String callingPath = System.getProperty("user.dir");
+			absoluteDirectory = callingPath+"\\"+relativeDir;
+		}
+		return absoluteDirectory;
 	}
 }
